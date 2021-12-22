8359da998941a6ed6c55d3c97913f2599ca83f0e
Jan Krysztop <jankrysztop@gmail.com>
2021-12-22 11:50:01 +0100

    Functionality added

---
+++ script.js
@@ -0,0 +1,25 @@
+setInterval(showTime, 1000)
+function showTime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let minute = time.getMinutes();
+  let second = time.getSeconds();
+  let isAmPm = "AM";
+
+  if (hour > 12) {
+    isAmPm = "PM";
+    hour = hour - 12;
+  }
+  if (hour == 0) {
+    isAmPm = "AM";
+  }
+
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+  second = second < 10 ? "0" + second : second;
+
+  let timeNow = hour + ":" + minute + ":" + second + " " + isAmPm;
+  document.querySelector("#clock").innerHTML = timeNow;
+}
+showTime();
+
